7985ff2b8457e03c26179d5b017b3568a738822a
XuHaoRan <no_signal@sjtu.edu.cn>
2025-11-11 11:13:33 +0800

    fix: 优化了遥控器解包时的互斥锁使用

--- User/Src/RemoteController.cpp
+++ User/Src/RemoteController.cpp
@@ -10,26 +10,28 @@ RemoteController::RemoteController(UART_HandleTypeDef* huart):
     huart_(huart) {}
 
 void RemoteController::parseData() {
-    osMutexAcquire(data_mutex_,osWaitForever); // 获取数据互斥锁
+    RCData data_copy;
     // 解包数据
-    data_.ch0 = normalize_channel(
+    data_copy.ch0 = normalize_channel(
         (static_cast<int16_t>(rx_data_[0]) | (static_cast<int16_t>(rx_data_[1]) << 8)) & 0x07FF);
-    data_.ch1 = normalize_channel(
+    data_copy.ch1 = normalize_channel(
         ((static_cast<int16_t>(rx_data_[1]) >> 3) | (static_cast<int16_t>(rx_data_[2]) << 5)) & 0x07FF);
-    data_.ch2 = normalize_channel(
+    data_copy.ch2 = normalize_channel(
         ((static_cast<int16_t>(rx_data_[2]) >> 6) | (static_cast<int16_t>(rx_data_[3]) << 2) | (static_cast<
             int16_t>(rx_data_[4]) << 10)) & 0x07FF);
-    data_.ch3 = normalize_channel(
+    data_copy.ch3 = normalize_channel(
         ((static_cast<int16_t>(rx_data_[4]) >> 1) | (static_cast<int16_t>(rx_data_[5]) << 7)) & 0x07FF);
-    data_.s1 = static_cast<Switch>(((rx_data_[5] >> 4) & 0x000C) >> 2);
-    data_.s2 = static_cast<Switch>((rx_data_[5] >> 4) & 0x0003);
-    data_.mouse_x = static_cast<int16_t>(rx_data_[6]) | (static_cast<int16_t>(rx_data_[7]) << 8);
-    data_.mouse_y = static_cast<int16_t>(rx_data_[8]) | (static_cast<int16_t>(rx_data_[9]) << 8);
-    data_.mouse_z = static_cast<int16_t>(rx_data_[10]) | (static_cast<int16_t>(rx_data_[11]) << 8);
-    data_.mouse_press_l = rx_data_[12];
-    data_.mouse_press_r = rx_data_[13];
-    data_.key = static_cast<int16_t>(rx_data_[14]) | (static_cast<int16_t>(rx_data_[15]) << 8);
+    data_copy.s1 = static_cast<Switch>(((rx_data_[5] >> 4) & 0x000C) >> 2);
+    data_copy.s2 = static_cast<Switch>((rx_data_[5] >> 4) & 0x0003);
+    data_copy.mouse_x = static_cast<int16_t>(rx_data_[6]) | (static_cast<int16_t>(rx_data_[7]) << 8);
+    data_copy.mouse_y = static_cast<int16_t>(rx_data_[8]) | (static_cast<int16_t>(rx_data_[9]) << 8);
+    data_copy.mouse_z = static_cast<int16_t>(rx_data_[10]) | (static_cast<int16_t>(rx_data_[11]) << 8);
+    data_copy.mouse_press_l = rx_data_[12];
+    data_copy.mouse_press_r = rx_data_[13];
+    data_copy.key = static_cast<int16_t>(rx_data_[14]) | (static_cast<int16_t>(rx_data_[15]) << 8);
 
+    osMutexAcquire(data_mutex_,osWaitForever); // 获取数据互斥锁
+    data_ = data_copy;
     osMutexRelease(data_mutex_);
 }
 
@@ -47,7 +49,6 @@ void RemoteController::taskEntry() {
 void RemoteController::RCInit(const osThreadAttr_t* thread_attr) {
     data_ready_semaphore_ = osSemaphoreNew(1, 0, nullptr); // 创建信号量
     data_mutex_ = osMutexNew(nullptr); // 创建互斥锁
-
     // 包装一下taskEntry() 以适应osThreadNew的参数要求
     auto wrapper = [](void* arg) {
         RemoteController* self = static_cast<RemoteController*>(arg);
