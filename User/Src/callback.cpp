ff620f870edaefaa66a5e0488ecfbe75f2de73bb
XuHaoRan <no_signal@sjtu.edu.cn>
2025-11-12 15:57:28 +0800

    feat: 完成了can_callback函数

--- User/Src/callback.cpp
+++ User/Src/callback.cpp
@@ -5,14 +5,21 @@
 #include "usart.h"
 #include "can.h"
 #include "RemoteController.h"
-
+#include "MotorManager.h"
 extern RemoteController remote_controller;
 
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan) {
     if (hcan == &hcan1) {
+        auto& motor_manager = MotorManager::instance();
         CAN_RxHeaderTypeDef message_header;
         uint8_t rx_data[8];
         HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &message_header, rx_data);
+        if (message_header.StdId >= 0x205 && message_header.StdId <= 0x20B) {
+            // 6020电机消息
+            auto motor_id = static_cast<uint8_t>(message_header.StdId - 0x204);
+            auto *motor = motor_manager.getMotorById(motor_id);
+            motor->CanRxCallback(rx_data);
+        }
     }
 }
 
